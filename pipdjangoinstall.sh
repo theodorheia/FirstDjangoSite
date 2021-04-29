0ecb8d5fe6bd7b35873817ab6f5f2a224bf45c95
theodorheia test machine <fl@moevideo.com>
2021-04-29 06:44:31 -0400

    Added bash-script for install Pip and Django

---
+++ pipdjangoinstall.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Install python3-pip....."
+sudo apt-get install python3-pip
+echo "Python3-pip installed sucsessfull!"
+sleep 3
+echo "Install Django"
+pip3 install django
+echo "Django installed sucsessfull!"
